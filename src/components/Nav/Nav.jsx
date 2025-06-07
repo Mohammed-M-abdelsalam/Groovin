047d5d51e61d9ea3a14ab0302632544e2f2db22d
Mohamed <mo.magdyabdelsalam@gmail.com>
2025-06-07 18:17:20 +0300

    trying to solve navbar problem on mobile

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function Nav(){
     const [active, setActive] = useState('home');
@@ -18,11 +18,11 @@ function Nav(){
     return(
         <nav className="bg-black opacity-50 transition-opacity duration-500 h-[75px] fixed right-0 left-0 top-0 z-50">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Groovin</span>
                 </a>
             <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button" className="text-white border-2 border-main hover:bg-main duration-500 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center">Get started</button>
+                <button type="button" className="text-white border-2 border-main hover:bg-main duration-500 focus:bg-main focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center">Get started</button>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
